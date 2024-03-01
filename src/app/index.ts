4937953e75683400ba673c9c4ac4511014616392
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-03-01 17:27:22 -0600

    removing stacktrace

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,17 +2,12 @@ import "reflect-metadata";
 import "./loader";
 import cors from "cors";
 import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
 import { server } from "./server";
 import createHttpError, { HttpError } from "http-errors";
 
 server
   .setConfig((app: express.Application) => {
-    app.set("env", "development");
     app.use(cors());
-    app.use(helmet());
-    app.use(morgan("dev"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   })
@@ -28,44 +23,22 @@ server
       }
     );
 
-    // error handlers
-    // development error handler
-    // will print stacktrace
-    if (_app.get("env") === "development") {
-      _app.use(
-        (
-          err: HttpError,
-          _req: express.Request,
-          res: express.Response,
-          _next: express.NextFunction
-        ): void => {
-          console.log("err", err);
-
-          res.status(err.status || 500);
-          res.json({
-            error: err,
-            message: err.message,
-          });
-        }
-      );
-    }
-
     // production error handler
     // no stacktraces leaked to user
-    // _app.use(
-    //   (
-    //     err: HttpError,
-    //     _req: express.Request,
-    //     res: express.Response,
-    //     _next: express.NextFunction
-    //   ): void => {
-    //     res.status(err.status || 500);
-    //     res.json({
-    //       error: {},
-    //       message: err.message,
-    //     });
-    //   }
-    // );
+    _app.use(
+      (
+        err: HttpError,
+        _req: express.Request,
+        res: express.Response,
+        _next: express.NextFunction
+      ): void => {
+        res.status(err.status || 500);
+        res.json({
+          error: {},
+          message: err.message,
+        });
+      }
+    );
   });
 
 const app = server.build();
