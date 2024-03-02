748ae08d54c5a31adf033beaf0d82c5c6c8b0200
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-03-01 18:16:26 -0600

    adding logger

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,10 +4,14 @@ import cors from "cors";
 import express from "express";
 import { server } from "./server";
 import createHttpError, { HttpError } from "http-errors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 server
   .setConfig((app: express.Application) => {
     app.use(cors());
+    app.use(helmet());
+    app.use(morgan("dev"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   })
